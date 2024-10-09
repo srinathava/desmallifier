42fb0d5c13cc296f72c8aa2a5f906666acdf1c08
Srinath Avadhanula <srinathava@gmail.com>
2024-10-08 23:14:49 -0400

    initial commit

---
+++ main.py
@@ -0,0 +1,196 @@
+import dxfgrabber
+from fpdf import FPDF
+import math
+from dataclasses import dataclass
+
+@dataclass
+class Params:
+    orientation: str = "landscape"
+    scale: float = 2.5
+    overlap: float = 0.5
+    margin: float = 0.25
+
+    def __post_init__(self):
+        if self.orientation == "landscape":
+            self.page_h = 8.5
+            self.page_w = 11
+        else:
+            self.page_h = 11
+            self.page_w = 8.5
+
+@dataclass
+class Point:
+    x: float = 0.0
+    y: float = 0.0
+
+    def offset(self, offset: 'Point'):
+        self.x = self.x - offset.x
+        self.y = self.y - offset.y
+
+@dataclass
+class RectXY:
+    bl: Point
+    tr: Point
+
+    def offset(self, offset: Point):
+        self.bl.offset(offset)
+        self.tr.offset(offset)
+
+@dataclass
+class Line:
+    start: Point
+    end: Point
+
+    @staticmethod
+    def from_dxf(e):
+        return Line(start=Point(e.start[0], e.start[1]), end=Point(e.end[0], e.end[1]))
+
+    def bounds(self) -> RectXY:
+        return RectXY(Point(min(self.start.x, self.end.x),
+                            min(self.start.y, self.end.y)),
+                      Point(max(self.start.x, self.end.x), 
+                            max(self.start.y, self.end.y)))
+
+    def offset(self, offset: Point):
+        self.start.offset(offset)
+        self.end.offset(offset)
+        
+@dataclass
+class Arc:
+    center: Point
+    r: float = 0.0
+    start_angle: float = 0.0
+    end_angle: float = 0.0
+
+    @staticmethod
+    def from_dxf(e):
+        return Arc(center= Point(e.center[0], e.center[1]), r=e.radius,
+                   start_angle=e.start_angle, end_angle=e.end_angle)
+
+    def bounds(self):
+        cx, cy = self.center.x, self.center.y
+        max_x = min_x = cx
+        max_y = min_y = cy
+
+        angles = [self.start_angle, self.end_angle]
+        wrap = self.start_angle > self.end_angle
+        for extra_angle in [0, 90, 180, 270]:
+            if ((self.start_angle <= extra_angle <= self.end_angle) or 
+                (wrap and extra_angle <= self.end_angle) or
+                (wrap and self.start_angle <= extra_angle)):
+                angles.append(extra_angle)
+
+        for angle in angles:
+            x = cx + self.r*math.cos(angle * math.pi/180)
+            y = cy + self.r*math.sin(angle * math.pi/180)
+            max_x = max(max_x, x)
+            max_y = max(max_y, y)
+            min_x = min(min_x, x)
+            min_y = min(min_y, y)
+
+        return RectXY(Point(min_x, min_y), Point(max_x, max_y))
+
+    def offset(self, offset: Point):
+        self.center.offset(offset)
+
+def update_bounds(bb1: RectXY, bb2: RectXY):
+    bl = Point(min(bb1.bl.x, bb2.bl.x), min(bb1.bl.y, bb2.bl.y))
+    tr = Point(max(bb1.tr.x, bb2.tr.x), max(bb1.tr.y, bb2.tr.y))
+    return RectXY(bl, tr)
+
+def draw_page(params: Params, entities, pdf, bb: Point, offset: Point):
+    ox = offset.x
+    oy = offset.y
+
+    def draw_line(x1, y1, x2, y2):
+        pdf.line(x1=params.scale*(x1+ox)+params.margin, 
+                 y1=params.page_h-params.scale*(y1+oy)-params.margin,
+                 x2=params.scale*(x2+ox)+params.margin,
+                 y2=params.page_h-params.scale*(y2+oy)-params.margin)
+
+    def draw_arc(e: Arc):
+        cx = params.scale*(e.center.x+ox)
+        cy = params.scale*(e.center.y+oy)
+        r = params.scale*e.r
+        pdf.arc(x=cx-r+params.margin, y=params.page_h-(cy+r)-params.margin, a=2*r, b=2*r,
+                end_angle=360-e.start_angle, start_angle=360-e.end_angle)
+
+    pdf.set_draw_color(r=255, g=150, b=150)
+
+    bb_r = min(bb.x, bb.y)
+    for i in range(math.ceil(params.scale*bb.x)+1):
+        for j in range(math.ceil(params.scale*bb.y)+1):
+            x1 = i/params.scale
+            y1 = j/params.scale
+            x2 = x1 + bb_r
+            y2 = y1 + bb_r
+            y3 = y1 - bb_r
+            draw_line(x1, y1, x2, y2)
+            draw_line(x1, y1, x2, y3)
+
+    pdf.set_draw_color(r=0, g=0, b=0)
+    for e in entities:
+        if isinstance(e, Line):
+            draw_line(e.start.x, e.start.y, e.end.x, e.end.y)
+        if isinstance(e, Arc):
+            draw_arc(e)
+
+    cutx = (params.page_w - params.overlap)/params.scale
+    cuty = (params.page_h - params.overlap)/params.scale
+    num_cutsx = math.ceil(bb.x/cutx)
+    num_cutsy = math.ceil(bb.y/cuty)
+    for i in range(num_cutsx):
+        for j in range(num_cutsy):
+            x = i*cutx
+            y = j*cuty
+            draw_line(x-0.25/params.scale, y, x+0.25/params.scale, y)
+            draw_line(x, y-0.25/params.scale, x, y+0.25/params.scale)
+
+def main():
+    dxf = dxfgrabber.readfile("test.dxf")
+    bb = RectXY(bl = Point(math.inf, math.inf), 
+                tr = Point(-math.inf, -math.inf))
+    entities = []
+    for e in dxf.entities:
+        if e.dxftype == 'LINE':
+            entity = Line.from_dxf(e)
+            entities.append(Line.from_dxf(e))
+        if e.dxftype == 'ARC':
+            entity = Arc.from_dxf(e)
+
+        entities.append(entity)
+        bb = update_bounds(bb, entity.bounds())
+
+    for e in entities:
+        e.offset(bb.bl)
+        print(e)
+
+    bb.offset(bb.bl)
+
+    pdf = FPDF(orientation="landscape", unit="in", format="letter")
+    pdf.set_line_width(0.5/25.4)
+    pdf.set_font("Helvetica", "B", 8) 
+
+    params = Params(overlap=1.0)
+
+    cutx = (params.page_w - params.overlap)/params.scale
+    cuty = (params.page_h - params.overlap)/params.scale
+    num_cutsx = math.floor(bb.tr.x/cutx)
+    num_cutsy = math.floor(bb.tr.y/cuty)
+    for i in range(num_cutsx):
+        for j in range(num_cutsy):
+            x = i*cutx
+            y = j*cuty
+
+            pdf.add_page()
+            with pdf.rect_clip(params.margin, params.margin, 
+                               params.page_w - 2*params.margin, 
+                               params.page_h - 2*params.margin):
+                draw_page(params, entities, pdf, bb.tr, Point(-x, -y))
+
+            pdf.text(0.125, 0.125, text=f'({i}, {j})')
+
+    pdf.output("test.pdf")
+
+# print(compute_arc_bounds(180, 90, (0, 0, 0), 1))
+main()
